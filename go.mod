4aa45dc91a22d8950a72f5ee67fe7165b910f950
BucaloL <bucalolana@gmail.com>
2024-01-23 12:03:28 +0100

    fix

--- go.mod
+++ go.mod
@@ -4,13 +4,13 @@ go 1.21.6
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.0
+	github.com/google/uuid v1.5.0
 	gorm.io/driver/postgres v1.5.4
 	gorm.io/gorm v1.25.5
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/google/uuid v1.5.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
--- model/user.go
+++ model/user.go
@@ -12,9 +12,15 @@ type User struct {
 	Email    string    `gorm:"uniqueIndex;not null" json:"email"`
 	Password string    `gorm:"not null" json:"password"`
 	Names    string    `json:"names"`
-	ID       uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
+	ID       uuid.UUID `gorm:"type:uuid;"`
 }
 
 type Users struct {
 	Users []User `json:"users"`
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	// UUID version 4
+	user.ID = uuid.New()
+	return
 }
