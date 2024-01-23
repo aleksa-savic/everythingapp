649c5bc328da84a21a129f729545841e52685e49
Maksim Mištec <maksimmistec@gmail.com>
2024-01-23 13:26:22 +0100

    feat: password is now hashed when creating user

--- api/controller/user_controller.go
+++ api/controller/user_controller.go
@@ -3,18 +3,16 @@ package controller
 import (
 	database "everythingapp/db"
 	"everythingapp/model"
+	"everythingapp/utils"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 )
 
-// delete user in db by ID
 func DeleteUserByID(c *fiber.Ctx) error {
 	db := database.DB
 	var user model.User
-	// get id params
 	id := c.Params("id")
-	// find single user in the database by id
 	db.Find(&user, "id = ?", id)
 	if user.ID == uuid.Nil {
 		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "User not found", "data": nil})
@@ -25,13 +23,23 @@ func DeleteUserByID(c *fiber.Ctx) error {
 	}
 	return c.Status(200).JSON(fiber.Map{"status": "success", "message": "User deleted"})
 }
+
 func CreateUser(c *fiber.Ctx) error {
 	db := database.DB
 	user := new(model.User)
+
+	hashedPassword, hash_err := utils.HashPassword(user.Password)
+	if hash_err != nil {
+		return hash_err
+	}
+
 	err := c.BodyParser(user)
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Something's wrong with your input", "data": err})
 	}
+
+	user.Password = hashedPassword
+
 	err = db.Create(&user).Error
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Could not create user", "data": err})
@@ -42,10 +50,10 @@ func CreateUser(c *fiber.Ctx) error {
 
 func GetAllUsers(c *fiber.Ctx) error {
 	db := database.DB
-	var users []model.User // find all users in the database
-	db.Find(&users)        // If no user found, return an error
+	var users []model.User
+	db.Find(&users)
 	if len(users) == 0 {
 		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Users not found", "data": nil})
-	} // return users
+	}
 	return c.Status(200).JSON(fiber.Map{"status": "sucess", "message": "Users Found", "data": users})
 }
